f15c80fe41c262756b3b6f6c57a64523e4b06722
fanciful-marmot <daniel.crookston@gmail.com>
2020-12-05 11:44:19 -0800

    Day 1 part 2
    I'm am not proud of this

--- day1/report-repair/src/main.rs
+++ day1/report-repair/src/main.rs
@@ -9,14 +9,8 @@ fn main() {
     let mut numbers = read_input(filename);
     numbers.sort();
 
-    for (i, x) in numbers.iter().enumerate() {
-        for y in numbers.iter().skip(i + 1) {
-            if x + y == 2020 {
-                println!("x: {}, y: {}, result: {}", x, y, x * y);
-                return;
-            }
-        }
-    }
+    println!("part 1: {}", part1(&numbers).unwrap());
+    println!("part 2: {}", part2(&numbers).unwrap());
 }
 
 fn read_input(filename: &str) -> Vec<u32> {
@@ -26,4 +20,33 @@ fn read_input(filename: &str) -> Vec<u32> {
         .lines()
         .map(|l| l.parse::<u32>().unwrap())
         .collect()
+}
+
+// Finds 2 numbers in the sorted input list that sum to 2020 and multiplies them together
+fn part1(values: &Vec<u32>) -> Option<u32> {
+    for (i, x) in values.iter().enumerate() {
+        for y in values.iter().skip(i + 1) {
+            if x + y == 2020 {
+                // println!("x: {}, y: {}, result: {}", x, y, x * y);
+                return Some(x * y);
+            }
+        }
+    }
+
+    return None;
+}
+
+fn part2(values: &Vec<u32>) -> Option<u32> {
+    for (i, x) in values.iter().enumerate() {
+        for (j, y) in values.iter().enumerate().skip(i + 1) {
+            for z in values.iter().skip(j + 1) {
+                if x + y + z == 2020 {
+                    // println!("x: {}, y: {}, result: {}", x, y, x * y);
+                    return Some(x * y * z);
+                }
+            }
+        }
+    }
+
+    return None;
 }
