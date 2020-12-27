ecbc1177173ad578dcddd65e89fb45d64a8beb8a
fanciful-marmot <daniel.crookston@gmail.com>
2020-12-27 14:24:13 -0800

    Da 13 - Part 2
    Well that was some math I haven't seen since first year...

--- day-13-shuttle-search/src/main.rs
+++ day-13-shuttle-search/src/main.rs
@@ -1,8 +1,8 @@
 use std::env;
 use std::fs;
 
-fn part_1(buses: &Vec<u32>, target_time: u32) -> u32 {
-    let (wait_time, bus) = buses.iter().fold((u32::MAX, u32::MAX), |acc, &bus| {
+fn part_1(buses: &Vec<(u64, u64)>, target_time: u64) -> u64 {
+    let (wait_time, bus) = buses.iter().fold((u64::MAX, u64::MAX), |acc, &(_, bus)| {
         let wait_time = bus * (target_time / bus + 1) - target_time;
         if wait_time < acc.0 {
             (wait_time, bus)
@@ -12,6 +12,47 @@ fn part_1(buses: &Vec<u32>, target_time: u32) -> u32 {
     });
 
     wait_time * bus
+}
+
+fn extended_gcd(mut a: i64, mut b: i64) -> (i64, i64) {
+    let (mut x, mut y, mut u, mut v) = (0, 1, 1, 0);
+
+    while a != 0 {
+        let (q, r) = (b / a, b % a);
+        let (m, n) = (x - u * q, y - v * q);
+
+        // After rust allows assigning in destructuring we shall be able to write
+        // (b,a,x,y,u,v) = (a,r,u,v,m,n)
+        b = a;
+        a = r;
+        x = u;
+        y = v;
+        u = m;
+        v = n;
+    }
+
+    (x, y)
+}
+
+fn part_2(buses: &Vec<(u64, u64)>) -> i64 {
+    // Apply chinese remainder theorem
+    // https://rosettacode.org/wiki/Chinese_remainder_theorem
+    // All input bus ids are coprime
+
+    let n: i64 = buses.iter().map(|&(_, id)| id as i64).product();
+
+    let x: i64 = buses
+        .iter()
+        .map(|&(index, id)| {
+            let ai = index as i64;
+            let n_over_ni = n / id as i64;
+            let (_, si) = extended_gcd(id as i64, n_over_ni);
+
+            ai * si * n_over_ni
+        })
+        .sum();
+
+    x.abs() % n
 }
 
 fn main() {
@@ -24,15 +65,18 @@ fn main() {
 
     let mut lines = contents.lines();
 
-    let target = lines.next().unwrap().parse::<u32>().unwrap();
+    let target = lines.next().unwrap().parse::<u64>().unwrap();
 
-    let buses: Vec<u32> = lines
+    // bus position, bus id
+    let buses: Vec<(u64, u64)> = lines
         .next()
         .unwrap()
         .split(",")
-        .filter(|&s| s != "x")
-        .map(|s| s.parse::<u32>().unwrap())
+        .enumerate()
+        .filter(|&(_, s)| s != "x")
+        .map(|(i, s)| (i as u64, s.parse::<u64>().unwrap()))
         .collect();
 
     println!("part 1: {}", part_1(&buses, target));
+    println!("part 2: {}", part_2(&buses));
 }
